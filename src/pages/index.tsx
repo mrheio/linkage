782c970fdc3966ef4dd6540f62d85414e6348637
mrheio <drg.ov00@gmail.com>
2023-09-17 00:58:04 +0300

    feat: List user communities on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { authService, communitiesService } from '~/services';
+import { CookieKey } from '~/utils';
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+	const accessToken = req.cookies[CookieKey.AccessToken];
+
+	if (!accessToken) {
+		return { props: {} };
+	}
+
+	const session = await authService.getSession(accessToken);
+	const communities = await communitiesService.getUserCommunities(session.id);
+
+	return { props: { communities: JSON.parse(JSON.stringify(communities)) } };
+};
+
+export default function Home(
+	props: InferGetServerSidePropsType<typeof getServerSideProps>,
+) {
+	const { communities } = props;
 	return (
 		<main>
 			<h1>This is my Home Page!</h1>
+			{communities?.map((c) => (
+				<article key={c.id}>
+					<h3>{c.name}</h3>
+					<p>{c.description}</p>
+					<small>
+						Created at:{' '}
+						{new Date(c.created_at).toLocaleDateString()}
+					</small>
+				</article>
+			))}
 		</main>
 	);
 }
