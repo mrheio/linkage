55a4c513228ed5c9802406c0b169d9847d1459d5
mrheio <drg.ov00@gmail.com>
2023-09-18 09:48:57 +0300

    feat: Display communities user is/is not on home-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,28 +10,64 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 	}
 
 	const session = await authService.getSession(accessToken);
-	const communities = await communitiesService.getUserCommunities(session.id);
+	const userCommunities = await communitiesService.getUserCommunities(
+		session.id,
+	);
+	const userNotInCommunities = await communitiesService.getUserCommunities(
+		session.id,
+		{ reverse: true },
+	);
 
-	return { props: { communities: JSON.parse(JSON.stringify(communities)) } };
+	return {
+		props: {
+			communities: JSON.parse(JSON.stringify(userCommunities)),
+			notInCommunities: JSON.parse(JSON.stringify(userNotInCommunities)),
+		},
+	};
 };
 
 export default function Home(
 	props: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) {
-	const { communities } = props;
+	const { communities, notInCommunities } = props;
+
 	return (
 		<main>
-			<h1>This is my Home Page!</h1>
-			{communities?.map((c) => (
-				<article key={c.id}>
-					<h3>{c.name}</h3>
-					<p>{c.description}</p>
-					<small>
-						Created at:{' '}
-						{new Date(c.created_at).toLocaleDateString()}
-					</small>
-				</article>
-			))}
+			<section>
+				<h1>Your communities</h1>
+				{communities?.map((c) => (
+					<article key={c.id}>
+						<header>
+							<h4>{c.name}</h4>
+						</header>
+						{c.description}
+						<footer>
+							<small>
+								Created at:{' '}
+								{new Date(c.created_at).toLocaleDateString()}
+							</small>
+						</footer>
+					</article>
+				))}
+			</section>
+
+			<section>
+				<h1>Not part of these ones yet</h1>
+				{notInCommunities?.map((c) => (
+					<article key={c.id}>
+						<header>
+							<h4>{c.name}</h4>
+						</header>
+						{c.description}
+						<footer>
+							<small>
+								Created at:{' '}
+								{new Date(c.created_at).toLocaleDateString()}
+							</small>
+						</footer>
+					</article>
+				))}
+			</section>
 		</main>
 	);
 }
